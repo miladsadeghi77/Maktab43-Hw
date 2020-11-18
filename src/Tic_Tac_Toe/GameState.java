308c7fad0d84ffb9d8557af3984c39363ccb9b63
miladSadeghi <miladpci88@gmail.com>
2020-11-18 20:42:29 +0330

    ticTacToe

---
+++ src/Tic_Tac_Toe/GameState.java
@@ -0,0 +1,5 @@
+package Tic_Tac_Toe;
+
+public enum GameState {  // to save as "GameState.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
---
+++ src/Tic_Tac_Toe/Seed.java
@@ -0,0 +1,5 @@
+package Tic_Tac_Toe;
+
+public enum Seed {
+    EMPTY, CROSS, NOUGHT
+}
